5652bb9358333e4ae58c054c87591a8ccd14e6b5
Tarociarz-z-Macedonii <olafwszeborowski123@gmail.com>
2025-04-20 20:14:03 +0200

    menu broni cz. 5

--- Scripts/weapon_menu_creator.gd
+++ Scripts/weapon_menu_creator.gd
@@ -2,22 +2,43 @@ extends TextureRect
 
 var weapon_array
 var menu
+var weapon_manager
+
 var button = preload("res://Objects/UI/WeaponMenu/button.tscn")
+var start_pos_y = 200
+var start_pos_x = 200
+var pos_x_gap = 150
+var button_size := Vector2(100,100)
+var buttons_made := false
 
 func _ready() -> void:
 	weapon_array = $/root/Main/WeaponManager/WeaponArray
 	menu =  $/root/Main/CanvasLayer/WeaponMenu
+	weapon_manager =  $/root/Main/WeaponManager
+	
+func _on_button_pressed(i):
+	weapon_manager.equip_weapon(weapon_array.keys[i])
 
-	
 func _make_buttons():
+	buttons_made = true
 	for i in weapon_array.weapons.size():
-		var temp = button.instantiate()	
-		$/root/Main.add_child(temp)
-	
+		var temp_button = button.instantiate()	
+		$/root/Main/CanvasLayer.add_child(temp_button)
+		var temp_weapon = weapon_array.weapons[weapon_array.keys[i]].instantiate()
+		$/root/Main.add_child(temp_weapon)
+		temp_button.texture_normal = temp_weapon.get_node('Sprite2D').texture 
+		temp_button.position = Vector2(start_pos_x + i * pos_x_gap, start_pos_y) 
+		temp_button.size = button_size
+		temp_button.pressed.connect(_on_button_pressed.bind(i))
+		temp_weapon.queue_free()
+		
 func _open_menu():
 	if Input.is_action_just_released("Open_Menu"):
+		if not buttons_made:
+			_make_buttons()
 		menu.visible = not menu.visible
-		_make_buttons()
+		
+		
 	
 func _process(delta: float) -> void:
 	_open_menu()
