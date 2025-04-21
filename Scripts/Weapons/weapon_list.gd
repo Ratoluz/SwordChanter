1e0a46c70cace02e379a38fee47b16ffa2c46c0f
Tarociarz-z-Macedonii <olafwszeborowski123@gmail.com>
2025-04-21 11:21:17 +0200

    Naming changes

---
+++ Scripts/Weapons/weapon_list.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var weapons = {}
+var keys = []
+
+func load_weapons():
+	weapons['Sword'] = load('res://Scenes/Resources/Weapons/Melee/Tier1/Sword.tres')
+	weapons['WaterWand'] = load('res://Scenes/Resources/Weapons/Magic/Tier1/WaterWand.tres')
+	keys = weapons.keys()
+
+func key_to_keyindex(key):
+	for i in keys.size():
+		if key == keys[i]:
+			return i
+	return -1
+	
+func give_next_keyindex(index):
+	if index >= keys.size() - 1:
+		return 0
+	return index + 1
