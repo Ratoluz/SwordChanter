fa4f9a813a07dffadff15762aa3a070feb4cda07
Tarociarz-z-Macedonii <olafwszeborowski123@gmail.com>
2025-05-06 14:41:04 +0200

    Deathscreen added

--- scripts/global/scene_manager.gd
+++ scripts/global/scene_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var current_dungeon = -1
 var dungeon_levels = []
 var first_scene = preload("res://scenes/places/main.tscn")
+var death_screen = preload('res://scenes/UI/death_screen.tscn')
 
 func _ready() -> void:
 	dungeon_levels.append(load("res://scenes/places/dungeon_1.tscn"))
@@ -14,4 +15,7 @@ func load_next_dungeon():
 
 func death():
 	current_dungeon = -1
+	get_tree().change_scene_to_packed(death_screen)
+
+func main_scene():
 	get_tree().change_scene_to_packed(first_scene)
