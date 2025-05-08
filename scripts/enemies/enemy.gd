315115d12a13efeed3145f979cb2c18481265da1
Tarociarz-z-Macedonii <olafwszeborowski123@gmail.com>
2025-05-08 10:10:18 +0200

    Enemy AI part 4
    added state transitions and flip cooldown, code is now cleaner

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -19,6 +19,7 @@ var spread: float
 var died := false
 var room
 var flipped := false
+var can_turn := true
 var state: EnemyState = EnemyState.CHASE
 var rand_pos: Vector2
 
@@ -27,6 +28,7 @@ var rand_pos: Vector2
 @onready var health_bar: ProgressBar = $HealthBar
 @onready var agent: NavigationAgent2D = $"NavigationAgent2D"
 @onready var shoot_cooldown: Timer = $"ShootCooldown"
+@onready var flip_cooldown: Timer = $"FlipCooldown"
 @onready var pick_random_pos_cooldown: Timer = $"PickRandomPosCooldown"
 @onready var animator: AnimatedSprite2D = $"AnimatedSprite2D"
 
@@ -42,9 +44,12 @@ func _ready() -> void:
 	
 	shoot_cooldown.timeout.connect(shoot)
 	shoot_cooldown.start()
+	shoot_cooldown.paused = true
 	pick_random_pos_cooldown.timeout.connect(_pick_random_pos)
 	pick_random_pos_cooldown.start()
-
+	pick_random_pos_cooldown.paused = true
+	flip_cooldown.timeout.connect(_on_filp_cooldown_timeout)
+	
 	agent.avoidance_enabled = true  # Turns on RVO
 	agent.radius = 70.0  # Set collision radius (adjust based on sprite size)
 	agent.neighbor_distance = 1000.0  # How far to check for other agents
@@ -53,8 +58,6 @@ func _ready() -> void:
 func _physics_process(_delta):
 	_flip()
 	_enemy_AI()
-	if died:
-		print('im dead but not hjahahaha')
 
 func _set_projectile_stats(temp_projectile):
 	speed = stats.speed
@@ -109,52 +112,87 @@ func drop() -> void:
 			get_tree().current_scene.call_deferred("add_child", drop_instance)
 
 func _flip():
+	if not can_turn: 
+		return
 	if not animator.flip_h and velocity.x < 0:
 		animator.flip_h = true
 		flipped = true
+		can_turn = false
+		flip_cooldown.start()
 		return
 	if animator.flip_h and velocity.x > 0:
 		animator.flip_h = false
 		flipped = false
+		can_turn = false
+		flip_cooldown.start()
+		
+func _on_filp_cooldown_timeout():
+	can_turn = true
+	flip_cooldown.stop()
 # ----------------ENEMY AI----------------------
 
 func _enemy_AI():
 	match state:
 		EnemyState.CHASE:
 			_on_state_chase()
+			_chase_to_walk_around()
 		EnemyState.WALK_AROUND:
 			_on_state_walk_around()
-
+			_walk_around_to_chase()
+# State Behavoiurs
 func _on_state_chase():
-	if pick_random_pos_cooldown.paused:
-		pick_random_pos_cooldown.paused = false
-	_follow_target(rand_pos)
-	
+	_follow_target(target.global_position)
+
 func _on_state_walk_around():
 	_follow_target(rand_pos)
+	print(pick_random_pos_cooldown.paused)
+
+# State Transitions
+func _to_walk_around():
+	_pick_random_pos()
+	pick_random_pos_cooldown.paused = false
+	shoot_cooldown.paused = false
+	state = EnemyState.WALK_AROUND
+	shoot()
+func _from_walk_around():
+	pick_random_pos_cooldown.paused = true
+	pick_random_pos_cooldown.wait_time = randf_range(0.5,0.6)
+	shoot_cooldown.paused = true
+	shoot_cooldown.wait_time = randf_range(cooldown_min, cooldown_max)
+
+func _to_chase():
+	state = EnemyState.CHASE
+func _from_chase():
+	pass
 	
+# ------------------------------	
 func _chase_to_walk_around():
-	var dist_to_player = roundi(global_position.distance_to(target.global_position) /6)
-	if dist_to_player == 30 and not state == EnemyState.WALK_AROUND:
-		state = EnemyState.WALK_AROUND
-	
+	var dist_to_player = global_position.distance_to(target.global_position)
+	if dist_to_player < 700:
+		_to_walk_around()
+		_from_chase()
+		
+func _walk_around_to_chase():
+	var dist_to_player = global_position.distance_to(target.global_position)
+	if dist_to_player > 700:
+		_to_chase()
+		_from_walk_around()
+		
+# State Actions
 func _pick_random_pos():
 	var dir = global_position.direction_to(target.global_position).angle()
 	var pos = target.global_position - Vector2(cos(dir) * 500, sin(dir) * 500)
-	var random_pos = Vector2(randi_range(-100, 100) + pos.x, randi_range(-100, 100) + pos.y)
+	var random_pos = Vector2(randi_range(-50, 50) + pos.x, randi_range(-100, 100) + pos.y)
 	rand_pos = random_pos 
-	pick_random_pos_cooldown.wait_time = randf_range(0.5,1.0)
-	print(target.global_position-random_pos)
+	pick_random_pos_cooldown.wait_time = randf_range(0.5,0.6)
 	
 func _follow_target(pos):
 	agent.target_position = pos
 	var direction = global_position.direction_to(agent.get_next_path_position())
 	agent.set_velocity(direction * speed) 
-	print(speed)
 	var adjusted_velocity = await agent.velocity_computed
 	velocity = adjusted_velocity
 	move_and_slide()
-	_chase_to_walk_around()
 
 func shoot():
 	var temp_projectile = projectile.instantiate()
